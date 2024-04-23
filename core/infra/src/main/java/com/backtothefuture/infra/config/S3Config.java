484824747d57b0f2c48bab0f90d7e346f92839ff
yuseon.lim <dogakday@gmail.com>
2024-04-23 23:17:52 +0900

    chore: action env 생성부분 분리, action test #141

--- core/infra/src/main/java/com/backtothefuture/infra/config/S3Config.java
+++ core/infra/src/main/java/com/backtothefuture/infra/config/S3Config.java
@@ -10,6 +10,7 @@ import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
+
 public class S3Config {
 
     @Value("${cloud.aws.s3.region}")
